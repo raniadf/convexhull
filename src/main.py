a883e9cb3a37fffde565a33a8f88874f4f82d7ac
raniadf <16520006@mahasiswa.itb.ac.id>
2022-02-26 21:58:10 +0700

    improve

--- src/main.py
+++ src/main.py
@@ -45,35 +45,47 @@ elif (namadata == 4) :
 
 # Hanya dapat mengakses file apabila memiliki column target
 if (punyatarget) :
-    # Menggunakan dataset sklearn
+    ## 3. INISIALISASI PYPLOT
+    plt.figure(figsize = (10, 6))
+    colors = ['b','r','g']
+    plt.title(label='Convex Hull', fontsize=20)
+
+    # Kasus apabila load dari database, simpan df terlebih dahulu
     if (namadata == 1 or namadata == 2 or namadata == 3) :
-        ## 3. MEMBUAT DATAFRAME
         df = pd.DataFrame(data.data, columns=data.feature_names)
         df['Target'] = pd.DataFrame(data.target)
-        col = len(df.columns) - 1
-        print("===========================================================")
-        print("Dataset ini terdiri atas ", col, "kolom, yaitu = ")
-        for i in range (col) :
-            print(i+1, data.feature_names[i])
-        print("===========================================================")
-        print("Silahkan pilih kolom acuan (dalam angka)!")
-        while (True) :
-            col1 = int(input("Acuan X = "))
-            if (col1 > 0 and col1 <= col) :
-                break
-        while (True) :
-            col2 = int(input("Acuan Y = "))
-            if (col2 > 0 and col2 <= col) :
-                break
 
-        ## 4. INISIALISASI PYPLOT
-        plt.figure(figsize = (10, 6))
-        colors = ['b','r','g']
-        plt.title(label='Convex Hull', fontsize=20)
+    ## 4. MEMINTA INPUT KOLOM ACUAN
+    column = df.columns.values.tolist()
+    col = len(df.columns) - 1
+    print("===========================================================")
+    print("Dataset ini terdiri atas ", col, "kolom, yaitu = ")
+    for i in range(col): 
+        if (namadata == 1 or namadata == 2 or namadata == 3) :
+            print(i+1, data.feature_names[i])
+        elif (namadata == 4) :
+            if (column[i] == "target") :
+                continue
+            print(i + 1, column[i])
+    print("===========================================================")
+    print("Silahkan pilih kolom acuan (dalam angka)!")
+    while (True) :
+        col1 = int(input("Acuan X = "))
+        if (col1 > 0 and col1 <= col) :
+            break
+    while (True) :
+        col2 = int(input("Acuan Y = "))
+        if (col2 > 0 and col2 <= col) :
+            break
+
+    ## 5. MENGOLAH DATA
+    # Menggunakan dataset dari sklearn
+    if (namadata == 1 or namadata == 2 or namadata == 3) :
+        ## 5.1 INISIALISASI LABEL
         plt.xlabel(data.feature_names[col1-1])
         plt.ylabel(data.feature_names[col2-1])
 
-        ## 5. MEMBUAT CONVEXHULL
+        ## 5.2 MEMBUAT CONVEXHULL
         for i in range(len(data.target_names)) :
             convexhull.convexIndex.clear()
             bucket = df[df['Target'] == i]
@@ -83,46 +95,22 @@ if (punyatarget) :
             for simplex in hull:
                 plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i%3])
 
-    # Menggunakan dataset csv`
+    # Menggunakan dataset csv
     elif (namadata == 4):
-        ## 3. MEMBUAT DATAFRAME
-        print("===========================================================")
-        print("Dataset ini terdiri atas ", len(df.columns) - 1, "kolom, yaitu = ")
-        col = df.columns.values.tolist()
-        for i in range(len(df.columns)): 
-            if (col[i] == "target") :
-                continue
-            print(i + 1, col[i])
-        print("===========================================================")
-        print("Silahkan pilih kolom acuan (dalam angka)!")
-        while (True) :
-            col1 = int(input("Acuan X = "))
-            if (col1 > 0 and col1 <= len(col)) :
-                break
-        while (True) :
-            col2 = int(input("Acuan Y = "))
-            if (col2 > 0 and col2 <= len(col)) :
-                break
-
-        ## 4. INISIALISASI PYPLOT
-        plt.figure(figsize = (10, 6))
-        colors = ['b','r','g']
-        plt.title(label='Convex Hull', fontsize=20)
-        plt.xlabel(col[col1-1])
-        plt.ylabel(col[col2-1])
+        ## 5.1 INISIALISASI LABEL
+        plt.xlabel(column[col1-1])
+        plt.ylabel(column[col2-1])
         target = df.target.unique()
 
-        ## 5. MEMBUAT CONVEXHULL
+        ## 5.2 MEMBUAT CONVEXHULL
         for i in range(len(target)) :
             convexhull.convexIndex.clear()
             bucket = df[df['target'] == target[i]]
             bucket = bucket.iloc[:,[col1-1,col2-1]].values
             hull = convexhull.myConvexHull(bucket)
-            P1, Pn = convexhull.extremePoints(bucket)
             plt.scatter(bucket[:, 0], bucket[:, 1], label=target[i])
             for simplex in hull:
                 plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i%3])
-
 
     ## 6. MENAMPILKAN VISUALISASI HASIL
     plt.legend()
